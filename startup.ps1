8d05fa9e6071cae9dd68941b72756ab2e02db0f5
Akash Kundu <akashkundu92@gmail.com>
2017-04-03 19:41:47 -0500

    Add files via upload

---
+++ startup.ps1
@@ -0,0 +1,19 @@
+﻿foreach($arg in $args)
+{
+    if($arg -eq "personal")
+    {
+       Start-Process -FilePath 'C:\Program Files (x86)\Notepad++\notepad++.exe'
+       Start-Process -FilePath 'C:\Windows\System32\cmd.exe'
+    } 
+    if($arg -eq "school")
+    {
+        Start-Process -FilePath 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe'
+        Start-Process -FilePath 'C:\Program Files (x86)\Notepad++\notepad++.exe'
+        Start-Process -FilePath 'C:\Users\Akash\Downloads\putty.exe'
+    }
+    if($arg -eq "start")
+    {
+        Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList 'gmail.com', 'ecampus.tamu.edu', 'piazza.com','http://courses.cse.tamu.edu/yum/Spring2017/312/', 'http://robotics.cs.tamu.edu/dshell/cs314/'
+
+    }
+}
